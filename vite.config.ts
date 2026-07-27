3efaf90e819692a22e5d52a87d6248f613e575d4
ashish-s-eq <designer.ashish@hotmail.com>
2026-07-28 01:10:36 +0530

    vite config modified

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/finance-flow/",
+  base: process.env.NODE_ENV === "production" ? "/finance-flow/" : "/",
   plugins: [react(), tailwindcss()],
 })
